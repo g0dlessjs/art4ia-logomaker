547df4d4247464487597baa1fe4be69be42c8a7a
g0dlessjs <juangalarce.dev@gmail.com>
2025-03-11 21:52:56 -0300

    Se crea una nueva seccion, se esta trabajando en ella

--- app/_data/Lookup.jsx
+++ app/_data/Lookup.jsx
@@ -5,4 +5,7 @@ export default {
     "Craft high-quality logos effortlessly with our innovative AI technology, tailored for startups, established brands, and digital ventures.",
   InputTitlePlaceholder: "Enter your Logo Name",
   GetStarted: "Get Started",
+  LogoTitle: "Logo Title",
+  LogoTitleDesc:
+    "Enter your business, app, or website name to create a personalized logo.",
 };
--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import LogoTitle from "./_components/LogoTitle";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 function CreateLogo() {
   return (
-    <div>
-      <h1>Create Logo</h1>
+    <div className="mt-28 p-10 border rounded-xl 2xl:mx-72">
+      <LogoTitle />
+
+      <div>
+        <Button variant="outline" className="cursor-pointer mx-2">
+          <ArrowLeft /> Previous
+        </Button>
+        <Button className="cursor-pointer">
+          <ArrowRight /> Continue
+        </Button>
+      </div>
     </div>
   );
 }
